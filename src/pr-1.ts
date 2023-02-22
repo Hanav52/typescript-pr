82a87726be8ab707f0c697127918aeeb505b9576
Hanav52 <wnsrl8329@gmail.com>
2023-02-23 02:18:13 +0900

    타입 정하기 3가지

---
+++ src/pr-1.ts
@@ -0,0 +1,29 @@
+    let a = [1,2];
+    let b : string[] = ["li", "1"];
+    let c : boolean[] = [true];
+// objesct의 타입을 명시적으로 정해주는 방법 3가지
+    type Age = number;
+    type Name = string;
+    type Player = {
+        name : Name,
+        age? : Age
+    }
+
+    const Nico : Player = {
+        name: "nico"
+    }
+
+    const lynn : Player = {
+        name:"lynn",
+        age:12
+    }
+    function playerMaker(name:string) : Player{
+        return {
+            name:name,
+        }
+    }
+    const playerMaker1 = (name:string) : Player => ({name})
+    const nico = playerMaker("nuco")
+    nico.age=12
+
+export {}
